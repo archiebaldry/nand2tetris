362fd5dae7365edc0492fb2a1c414a55e35c32eb
archiebaldry <archiebaldry@protonmail.com>
2021-07-08 16:01:52 +0100

    feat: Implement multiplication in Hack assembly

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,43 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// product <- R2
+// inc <- R0
+// count <- R1
+// 
+// # SWAP INC & COUNT
+// # if R1 > R0:
+// #    inc <- R1
+// #    count <- R0
+//
+// for i in count:
+//     product <- product + inc
+
+@R2
+M=0 // R2 <- 0
+
+@i
+M=0 // i <- 0
+
+(LOOP)
+    @i
+    D=M // Data <- i
+    @R1
+    D=M-D // Data <- R1 (count) - Data (i)
+    @END
+    D;JEQ // Goto END if Data = 0 (Goto END if i = count)
+
+    @R0
+    D=M // Data <- R0 (inc)
+    @R2
+    M=M+D // R2 <- R2 + Data (inc)
+
+    @i
+    M=M+1 // i <- i + 1
+
+    @LOOP
+    0;JMP // Goto LOOP
+
+(END)
+    @END
+    0;JMP
