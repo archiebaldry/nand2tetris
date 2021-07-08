a8c4f947a4f370b96c9ee2a09d22ba0b9e838c68
archiebaldry <archiebaldry@protonmail.com>
2021-07-08 16:37:26 +0100

    refactor: Fewer cycles
    In Mult.asm if R1 > R0

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -27,13 +27,27 @@ M=0 // R2 <- 0
 @i
 M=0 // i <- 0
 
+// Load R1 into temp
+@R1
+D=M // Data <- R1
+@temp
+M=D // temp <- Data (R1)
+
+// if R1 > R0 ___ if (R1 - R0) > 0 then
+//     set R1 to R0
+//     set R0 to temp (R1)
+@R0
+D=D-M // Data <- Data (R1) - R0
+@SWAP
+D;JGT // Goto SWAP if Data > 0
+
 (LOOP)
     @i
     D=M // Data <- i
     @R1
     D=M-D // Data <- R1 (count) - Data (i)
     @END
-    D;JEQ // Goto END if Data = 0 (Goto END if i = count)
+    D;JEQ // Goto END if Data = 0 ___ Goto END if i = count
 
     @R0
     D=M // Data <- R0 (inc)
@@ -43,6 +57,18 @@ M=0 // i <- 0
     @i
     M=M+1 // i <- i + 1
 
+    @LOOP
+    0;JMP // Goto LOOP
+
+(SWAP)
+    @R0
+    D=M // Data <- R0
+    @R1
+    M=D // R1 <- Data (R0)
+    @temp
+    D=M // Data <- temp
+    @R0
+    M=D // R0 <- Data (temp)
     @LOOP
     0;JMP // Goto LOOP
 
