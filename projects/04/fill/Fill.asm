a06ec11cc44e863e4da506cbf240693c33f5f760
archiebaldry <archiebaldry@protonmail.com>
2021-07-14 20:53:47 +0100

    feat: Implement fill in Hack assembly

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,42 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(INPUT)
+    @SCREEN
+    D=A // Data = 16384
+    @i
+    M=D // i = Data
+    @KBD
+    D=M // Data = Keyboard
+    @CLEAR
+    D;JEQ // Jump to CLEAR if Data == 0
+    @FILL
+    0;JMP // Jump to FILL
+
+(CLEAR)
+    @i
+    A=M // Address = i
+    M=0 // @i = 0000000000000000 (2's complement)
+    @i
+    M=M+1 // i += 1
+    D=M // Data = i
+    @KBD
+    D=D-A // Data -= 24576 
+    @INPUT
+    D;JEQ // Jump to INPUT if Data == 0
+    @CLEAR
+    0;JMP // Jump to CLEAR
+
+(FILL)
+    @i
+    A=M // Address = i
+    M=-1 // @i = 1111111111111111 (2's complement)
+    @i
+    M=M+1 // i += 1
+    D=M // Data = i
+    @KBD
+    D=D-A // Data -= 24576 
+    @INPUT
+    D;JEQ // Jump to INPUT if Data == 0
+    @FILL
+    0;JMP // Jump to FILL
